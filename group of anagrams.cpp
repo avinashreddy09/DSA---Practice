65b9be55b34a25c82f26591b0c5b915a76fe335a
Avinash Reddy <153441185+avinashreddy09@users.noreply.github.com>
2026-02-07 18:13:54 +0530

    Add files via upload

---
+++ group of anagrams.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public: vector < vector < string >> groupAnagrams(vector < string > & strs) {
+        unordered_map < string, vector < string >> mp;
+        for (string s: strs) {
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            mp[sorted].push_back(s);
+        }
+        vector < vector < string >> result;
+        for (auto & pair: mp) {
+            result.push_back(pair.second);
+        }
+        return result;
+    }
+};
